b2abc287216f5137b4bacec4909c599d6586949c
Alpha11111 <xiongpeng@baiyanglin.cn>
2016-05-26 13:59:46 +0800

    update

--- wordpress/wp-content/themes/iconntechs/page.php
+++ wordpress/wp-content/themes/iconntechs/page.php
@@ -9,7 +9,7 @@ if(is_checkout() || is_page('My Account')){
 }
 if(is_page('My Account') and !$user_ID){
 	$aurl = get_permalink();
-	wp_redirect( site_url('/wp-login.php?redirect_to=$aurl"'));
+	wp_redirect( site_url("/wp-login.php?redirect_to=$aurl"));
 	//header("Location:".bloginfo('home')."/wp-login.php?redirect_to=$aurl");die;
 }
 ?>
