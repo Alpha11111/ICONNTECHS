eedf069b4c1c9560d64f73f131290a8be48b45b6
Alpha11111 <xiongpeng@baiyanglin.cn>
2016-06-07 15:43:19 +0800

    update

--- wordpress/wp-content/themes/iconntechs/cart.php
+++ wordpress/wp-content/themes/iconntechs/cart.php
@@ -185,8 +185,7 @@ if(!empty($_POST)){
       </div><!-- /.modal-content -->
    </div><!-- /.modal-dialog -->
 </div><!-- /.modal -->
-		<script src="<?php bloginfo('template_url'); ?>/bootstrap-3.3.5-dist/js/jquery-1.11.0.js"></script>
-		<script src="<?php bloginfo('template_url'); ?>/bootstrap-3.3.5-dist/js/bootstrap.js"></script>
+		
 		<script>
 				$(function(){
 					var id=0;
