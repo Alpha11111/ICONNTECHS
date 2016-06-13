eea50de0e44e6151e826b560b46c43011802bf65
Alpha11111 <xiongpeng@baiyanglin.cn>
2016-06-13 11:34:29 +0800

    update

--- wordpress/wp-content/themes/iconntechs/head-shop.php
+++ wordpress/wp-content/themes/iconntechs/head-shop.php
@@ -38,6 +38,16 @@ $carnum = count($carnum);
 	  ga('send', 'pageview');
 
 	</script>
+	<script type="text/javascript">
+ 		var _gaq = _gaq || [];
+  		_gaq.push(['_setAccount', 'UA-74879058-2']);
+ 	 	_gaq.push(['_trackPageview']);
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
 
 		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
--- wordpress/wp-content/themes/iconntechs/woocommerce-shop.php
+++ wordpress/wp-content/themes/iconntechs/woocommerce-shop.php
@@ -124,9 +124,19 @@ if(!empty($_POST['pnum'])){
 	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-	  var _gaq = _gaq || [];
+	  	
 	  ga('create', 'UA-74879058-2', 'auto');
 	  ga('send', 'pageview');
+	</script>
+<script type="text/javascript">
+ 		var _gaq = _gaq || [];
+  		_gaq.push(['_setAccount', 'UA-74879058-2']);
+ 	 	_gaq.push(['_trackPageview']);
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
 
 	</script>
 	</head>
