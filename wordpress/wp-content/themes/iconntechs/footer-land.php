e9a87186fbd26b5d2ee4d7610801d0233e910fbe
Alpha11111 <xiongpeng@baiyanglin.cn>
2016-06-21 14:30:59 +0800

    update

--- wordpress/wp-content/themes/iconntechs/footer-land.php
+++
@@ -1,6 +0,0 @@
-<div id="footer">
-		  	  	   <p>
-		  	  	   	<a class="faceBook" href="https://business.facebook.com/iconntechs/?business_id=159600407712495" target="_blank"></a>&nbsp; &nbsp; &nbsp; 
-		  	  	   	<a class="twitter" href="https://twitter.com/Iconntechs1" target="_blank"></a></p>
-		  	  	   <p>© 2016 ICONNECHS.com</p>
-</div>
